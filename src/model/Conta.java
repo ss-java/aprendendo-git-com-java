aebd037b5ed1379594f88e4ff7a935382e330d67
PauloEF <pauloestevao.ferreira@hotmail.com>
2019-05-04 12:29:59 -0300

    Criado Metodos Sacar e Depositar

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -5,12 +5,17 @@ public class Conta {
 	 * A agência onde a conta está cadastrada.
 	 */
 	private Agencia agencia;
-	
+
 	/**
 	 * O número da conta.
 	 */
 	private String numero;
 	
+	/**
+	 * O valor contido na conta. 
+	 */
+	private double saldo;
+
 	/**
 	 * Construtor.
 	 * 
@@ -20,5 +25,35 @@ public class Conta {
 	public Conta(Agencia agencia, String numero) {
 		this.agencia = agencia;
 		this.numero = numero;
+		this.saldo = 0;
+
 	}
+	
+	public double GetSaldo() {
+		
+		return this.saldo;
+	}
+
+	
+
+	/**
+	 *  metodo para sacar
+	 * @param valor
+	 */
+	public void sacar(double valor) {
+		this.saldo = this.saldo - valor;
+
+	}
+
+	/**
+	 *  metodo para depositar
+	 * @param valor
+	 */
+	public void depositar(double valor) {
+		this.saldo = this.saldo + valor;
+
+	}
+	
+	
+
 }
